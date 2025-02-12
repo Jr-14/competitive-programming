f770b87b331507021b8c09eb34d86dcc7aa203fd
John Emerson Requizo <jr.jemerson14@gmail.com>
2025-02-12 21:11:19 +1100

    Initial file for e

---
+++ practice/atcoder/abc388/e.cpp
@@ -0,0 +1,35 @@
+// https://atcoder.jp/contests/abc388/tasks/abc388_e
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+/**
+ * Speed up cin and cout
+ */
+void Speed() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+void Solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+}
+
+
+int main() {
+    Speed();
+    int t = 1;
+    while (t--) {
+        Solve();
+    }
+    return 0;
+}
